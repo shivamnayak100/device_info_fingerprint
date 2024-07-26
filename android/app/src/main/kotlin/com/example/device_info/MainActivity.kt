33f170692d926935d88b004f0ed95e150e95d03c
Shivam Kumar Nayak <snayak@datopic.com>
2024-07-26 21:57:57 +0530

    geting more information

--- android/app/src/main/kotlin/com/example/device_info/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/device_info/MainActivity.kt
@@ -14,6 +14,10 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import org.json.JSONObject
+import android.os.BatteryManager
+import android.content.Intent
+import android.content.IntentFilter
+
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "device_info"
@@ -25,10 +29,6 @@ class MainActivity: FlutterActivity() {
                 "getDeviceInfo" -> {
                     val deviceId = fetchDeviceId()
                     result.success(deviceId)
-                }
-                "getBatteryInfo" -> {
-                    val batteryInfo = fetchBatteryInfo()
-                    result.success(batteryInfo)
                 }
                 "getRamInfo" -> {
                     val ramInfo = fetchRamInfo()
@@ -46,6 +46,14 @@ class MainActivity: FlutterActivity() {
                     val manufacturerInfo = fetchManufacturerInfo()
                     result.success(manufacturerInfo)
                 }
+                "versionInfo" -> {
+                    val versionInfo = fetchBuildVersionInfo()
+                    result.success(versionInfo)
+                }
+                "getBatteryInfo" -> {
+                    val batteryInfo = fetchBatteryInfo()
+                    result.success(batteryInfo)
+                }
                 else -> result.notImplemented()
             }
         }
@@ -55,10 +63,59 @@ class MainActivity: FlutterActivity() {
         return Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
     }
 
-    private fun fetchBatteryInfo(): String {
-        val batteryManager = getSystemService(Context.BATTERY_SERVICE) as android.os.BatteryManager
-        val batteryLevel = batteryManager.getIntProperty(android.os.BatteryManager.BATTERY_PROPERTY_CAPACITY)
-        return "Battery Level: $batteryLevel%"
+   private fun fetchBatteryInfo(): String {
+        val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+        val batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        val batteryChargeCounter = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER)
+        val batteryCurrentAverage = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_AVERAGE)
+        val batteryCurrentNow = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
+        val batteryEnergyCounter = batteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER)
+        val batteryHealth = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_STATUS)
+        val batteryStatus = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_STATUS)
+        val isCharging = batteryManager.isCharging
+
+        val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        val batteryStatusIntent = registerReceiver(null, intentFilter)
+        val batteryVoltage = batteryStatusIntent?.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1)
+        val batteryTemperature = batteryStatusIntent?.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1)
+
+        val batteryInfo = mapOf(
+            "Battery Level" to "$batteryLevel%",
+            "Battery Charge Counter" to "$batteryChargeCounter",
+            "Battery Current Average" to "$batteryCurrentAverage µA",
+            "Battery Current Now" to "$batteryCurrentNow µA",
+            "Battery Energy Counter" to "$batteryEnergyCounter nWh",
+            "Battery Health" to getBatteryHealthString(batteryHealth),
+            "Battery Voltage" to "$batteryVoltage mV",
+            "Battery Temperature" to "${batteryTemperature?.div(10.0)} °C",
+            "Battery Status" to getBatteryStatusString(batteryStatus),
+            "Is Charging" to isCharging.toString()
+        )
+        return JSONObject(batteryInfo).toString()
+    }
+
+    private fun getBatteryHealthString(health: Int): String {
+        return when (health) {
+            BatteryManager.BATTERY_HEALTH_COLD -> "Cold"
+            BatteryManager.BATTERY_HEALTH_DEAD -> "Dead"
+            BatteryManager.BATTERY_HEALTH_GOOD -> "Good"
+            BatteryManager.BATTERY_HEALTH_OVERHEAT -> "Overheat"
+            BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE -> "Over Voltage"
+            BatteryManager.BATTERY_HEALTH_UNKNOWN -> "Unknown"
+            BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE -> "Unspecified Failure"
+            else -> "Unknown"
+        }
+    }
+
+    private fun getBatteryStatusString(status: Int): String {
+        return when (status) {
+            BatteryManager.BATTERY_STATUS_CHARGING -> "Charging"
+            BatteryManager.BATTERY_STATUS_DISCHARGING -> "Discharging"
+            BatteryManager.BATTERY_STATUS_FULL -> "Full"
+            BatteryManager.BATTERY_STATUS_NOT_CHARGING -> "Not Charging"
+            BatteryManager.BATTERY_STATUS_UNKNOWN -> "Unknown"
+            else -> "Unknown"
+        }
     }
 
     private fun fetchRamInfo(): String {
@@ -85,11 +142,47 @@ class MainActivity: FlutterActivity() {
         return "Cameras: $cameras"
     }
 
+    //******************************************** */
+
     private fun fetchManufacturerInfo(): String {
-        val manufacturerInfo = mapOf(
-            "Manufacturer" to Build.MANUFACTURER,
-            "Model" to Build.MODEL
+        val buildInfo = mapOf(
+            "BOARD" to Build.BOARD,
+            "BOOTLOADER" to Build.BOOTLOADER,
+            "BRAND" to Build.BRAND,
+            "DEVICE" to Build.DEVICE,
+            "DISPLAY" to Build.DISPLAY,
+            "FINGERPRINT" to Build.FINGERPRINT,
+            "HARDWARE" to Build.HARDWARE,
+            "HOST" to Build.HOST,
+            "ID" to Build.ID,
+            "MANUFACTURER" to Build.MANUFACTURER,
+            "MODEL" to Build.MODEL,
+            "PRODUCT" to Build.PRODUCT,
+            "TAGS" to Build.TAGS,
+            "TIME" to Build.TIME.toString(),
+            "TYPE" to Build.TYPE,
+            "USER" to Build.USER,
+            "RADIO" to Build.RADIO,
+            "SERIAL" to Build.SERIAL,
+            "SUPPORTED_32_BIT_ABIS" to Build.SUPPORTED_32_BIT_ABIS.joinToString(", "),
+            "SUPPORTED_64_BIT_ABIS" to Build.SUPPORTED_64_BIT_ABIS.joinToString(", "),
+            "SUPPORTED_ABIS" to Build.SUPPORTED_ABIS.joinToString(", ")
         )
-        return JSONObject(manufacturerInfo).toString()
+        return JSONObject(buildInfo).toString()
+    }
+
+    // ********************************************* //
+    private fun fetchBuildVersionInfo(): String {
+        val versionInfo = mapOf(
+            "BASE_OS" to Build.VERSION.BASE_OS,
+            "CODENAME" to Build.VERSION.CODENAME,
+            "INCREMENTAL" to Build.VERSION.INCREMENTAL,
+            "PREVIEW_SDK_INT" to Build.VERSION.PREVIEW_SDK_INT.toString(),
+            "RELEASE" to Build.VERSION.RELEASE,
+            "SDK" to Build.VERSION.SDK,
+            "SDK_INT" to Build.VERSION.SDK_INT.toString(),
+            "SECURITY_PATCH" to Build.VERSION.SECURITY_PATCH
+        )
+        return JSONObject(versionInfo).toString()
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -22,26 +22,26 @@ class DeviceInfoScreen extends StatefulWidget {
 
 class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
   static const platform = MethodChannel('device_info');
-  String _deviceId = 'Unknown';
-  String _batteryInfo = 'Unknown';
+  String _deviceId = "Unknow";
   String _ramInfo = 'Unknown';
   String _cpuInfo = 'Unknown';
   String _cameraInfo = 'Unknown';
-  Map<String, String> _manufacturerInfo = {'Manufacturer': 'Unknown', 'Model': 'Unknown'};
+  Map<String, String> _manufacturerInfo = {};
+  Map<String, String> _versionInfo = {};
+  Map<String, String> _batteryInfo = {};
 
   Future<void> _getDeviceInfo() async {
     String deviceId;
-    String batteryInfo;
     String ramInfo;
     String cpuInfo;
     String cameraInfo;
     Map<String, String> manufacturerInfo;
+    Map<String, String> versionInfo;
+    Map<String, String> batteryInfo;
     try {
       final String result = await platform.invokeMethod('getDeviceInfo');
       deviceId = 'Device ID: $result';
 
-      final String batteryResult = await platform.invokeMethod('getBatteryInfo');
-      batteryInfo = batteryResult;
 
       final String ramResult = await platform.invokeMethod('getRamInfo');
       ramInfo = ramResult;
@@ -54,13 +54,21 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
 
       final String manufacturerResult = await platform.invokeMethod('getManufacturerInfo');
       manufacturerInfo = Map<String, String>.from(json.decode(manufacturerResult));
+
+      final String versionInfoResult = await platform.invokeMethod('versionInfo');
+      versionInfo = Map<String, String>.from(json.decode(versionInfoResult));
+
+      final String batterInfoResult = await platform.invokeMethod('getBatteryInfo');
+      batteryInfo = Map<String, String>.from(json.decode(batterInfoResult));
+
     } on PlatformException catch (e) {
       deviceId = "Failed to get device info: '${e.message}'.";
-      batteryInfo = "Failed to get battery info: '${e.message}'.";
       ramInfo = "Failed to get RAM info: '${e.message}'.";
       cpuInfo = "Failed to get CPU info: '${e.message}'.";
       cameraInfo = "Failed to get camera info: '${e.message}'.";
-      manufacturerInfo = {'Manufacturer': "Failed to get manufacturer info", 'Model': '${e.message}'};
+      manufacturerInfo = {'MANUFACTURER':'${e.message}'};
+      versionInfo = {"BASE_OS": '${e.message}'};
+      batteryInfo = {'Error': "Failed to get battery info: '${e.message}'"};
     }
 
     setState(() {
@@ -70,6 +78,7 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
       _cpuInfo = cpuInfo;
       _cameraInfo = cameraInfo;
       _manufacturerInfo = manufacturerInfo;
+      _versionInfo = versionInfo;
     });
   }
 
@@ -79,23 +88,69 @@ class _DeviceInfoScreenState extends State<DeviceInfoScreen> {
       appBar: AppBar(
         title: Text('Device Info'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(_deviceId),
-            Text(_batteryInfo),
-            Text(_ramInfo),
-            Text(_cpuInfo),
-            Text(_cameraInfo),
-            Text('Manufacturer: ${_manufacturerInfo['Manufacturer']}'),
-            Text('Model: ${_manufacturerInfo['Model']}'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _getDeviceInfo,
-              child: Text('Get Device Info'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _getDeviceInfo,
+                  child: Text('Get Device Info'),
+                ),
+                const SizedBox(height: 10,),
+                Text(_deviceId),
+                Text(_ramInfo),
+                Text(_cpuInfo),
+                Text(_cameraInfo),
+
+                // %%%%%%%%%%%%%%%%%%%%%%%
+                const SizedBox(height: 10,),
+                const Text('Manufacture Info:- ', style: TextStyle(fontWeight: FontWeight.bold),),
+
+                Text('MANUFACTURER: ${_manufacturerInfo['MANUFACTURER']}'),
+                Text('MODEL: ${_manufacturerInfo['MODEL']}'),
+                Text('BOARD: ${_manufacturerInfo['BOARD']}'),
+                Text('BOOTLOADER: ${_manufacturerInfo['BOOTLOADER']}'),
+                Text('BRAND: ${_manufacturerInfo['BRAND']}'),
+                Text('DEVICE: ${_manufacturerInfo['DEVICE']}'),
+                Text('DISPLAY: ${_manufacturerInfo['DISPLAY']}'),
+                Text('FINGERPRINT: ${_manufacturerInfo['FINGERPRINT']}'),
+                Text('HARDWARE: ${_manufacturerInfo['HARDWARE']}'),
+                Text('HOST: ${_manufacturerInfo['HOST']}'),
+                Text('ID: ${_manufacturerInfo['ID']}'),
+                Text('PRODUCT: ${_manufacturerInfo['PRODUCT']}'),
+                Text('TAGS: ${_manufacturerInfo['TAGS']}'),
+                Text('TIME: ${_manufacturerInfo['TIME']}'),
+                Text('TYPE: ${_manufacturerInfo['TYPE']}'),
+                Text('USER: ${_manufacturerInfo['USER']}'),
+                Text('RADIO: ${_manufacturerInfo['RADIO']}'),
+                Text('SERIAL: ${_manufacturerInfo['SERIAL']}'),
+                Text('SUPPORTED_32_BIT_ABIS: ${_manufacturerInfo['SUPPORTED_32_BIT_ABIS']}'),
+                Text('SUPPORTED_64_BIT_ABIS: ${_manufacturerInfo['SUPPORTED_64_BIT_ABIS']}'),
+                Text('SUPPORTED_ABIS: ${_manufacturerInfo['SUPPORTED_ABIS']}'),
+
+                // %%%%%%%%%%%%%%%%%%%%%%%
+                const SizedBox(height: 10,),
+                const Text('Version Info:- ', style: TextStyle(fontWeight: FontWeight.bold),),
+                Text('BASE_OS: ${_versionInfo['BASE_OS']}'),
+                Text('CODENAME: ${_versionInfo['CODENAME']}'),
+                Text('INCREMENTAL: ${_versionInfo['INCREMENTAL']}'),
+                Text('PREVIEW_SDK_INT: ${_versionInfo['PREVIEW_SDK_INT']}'),
+                Text('RELEASE: ${_versionInfo['RELEASE']}'),
+                Text('SDK: ${_versionInfo['SDK']}'),
+                Text('SDK_INT: ${_versionInfo['SDK_INT']}'),
+                Text('SECURITY_PATCH: ${_versionInfo['SECURITY_PATCH']}'),
+
+                // %%%%%%%%%%%%%%%%%%%%%%%
+                const SizedBox(height: 10,),
+                const Text('Battery Info:- ', style: TextStyle(fontWeight: FontWeight.bold),),
+                ..._batteryInfo.entries.map((entry) => Text('${entry.key}: ${entry.value}')).toList(),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
